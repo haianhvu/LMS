0e47460697f51ae847d116aa0f5ee3ce82796770
YourUsername <haianhvu@ueh.edu.vn>
2018-11-02 00:25:59 +0100

    First time Upload

---
+++ scholarship2.R
@@ -0,0 +1,118 @@
+install.packages("readr")
+install.packages("dplyr")
+
+library("readr")
+library("dplyr")
+
+rm(list = ls())
+setwd("C:/Users/Vu/Google Drive/Ph.D/LMS")
+
+# Import data into R
+setwd("C:/Users/Vu/Google Drive/Ph.D/LMS")
+KKHT <- read_delim(file = "Ket-qua-HB-KKHT-UEH-HKD-2017.csv", delim=',')
+HTHT <- read_delim(file = "Ket-qua-HB-HTHT-UEH-HKD-2017.csv", delim=',')
+
+# Change the format
+names(HTHT) <- c("Stt","Khoa","MSSV","Lop","Ho","Ten","Ngay sinh","dtbhb","ren luyen","cmnd","%da cap","%HTHT","%KKHT","%HTHT+KKHT","Se nhan","so tien","chi nhan HTHT")
+names(KKHT) <- c("Stt","Khoa","MSSV","Lop","Ho","Ten","Ngay sinh","dtbhb","ren luyen","cmnd","%da cap","%HTHT","%KKHT","%HTHT+KKHT","Se nhan","so tien","chi nhan KKHT")
+KKHT$`ren luyen` <- as.numeric(sub(",", ".", KKHT$`ren luyen`, fixed = TRUE))
+KKHT$dtbhb <- ifelse(KKHT$dtbhb > 100, KKHT$dtbhb/100, KKHT$dtbhb/10)
+HTHT$dtbhb <- ifelse(HTHT$dtbhb > 100, HTHT$dtbhb/100, HTHT$dtbhb/10)
+
+# Keep the K42 student only, 516 and 162
+KKHT42 <- KKHT[KKHT$Khoa==42,]
+length(KKHT42$MSSV)
+HTHT42 <- HTHT[HTHT$Khoa==42,]
+length(HTHT42$MSSV)
+
+# Test how many k42 students receive 2 types of scholarship
+length(which(KKHT42$`chi nhan KKHT`==0))
+sum(HTHT42$`chi nhan HTHT`==0)      
+
+# Retrieve k42 students having 2 scholarships
+kk_sch <- KKHT42[KKHT42$`chi nhan KKHT`==0,][,c(3,11:14)] # 26 students
+ht_sch <- HTHT42[HTHT42$`chi nhan HTHT`==0,][,c(3,11:14)] # 18 students
+
+# Why 26 and 18? They should be equal. Answer: 100% KKHT and 100% HTHT
+sch2 <- rbind(kk_sch,ht_sch)
+#sch2 <- sch2[order(sch2$MSSV),]
+#test <- sch2[!(duplicated(sch2$MSSV) | duplicated(sch2$MSSV, fromLast = TRUE)), ]
+indDuplicatedVec <- duplicated(sch2$MSSV) | duplicated(sch2$MSSV, fromLast = TRUE)
+xuathien2hb <- sch2[indDuplicatedVec,]
+xuathien1hb <- sch2[!indDuplicatedVec,]
+
+
+# ------------------- Diem thi dau vao -------------------
+
+# # Input the data
+# setwd("C:/Users/Vu/Google Drive/Ph.D/LMS")
+# diemthi <- read_delim(file = "K42-Diem.csv", delim=',')
+# 
+# # Input data student info
+# studentinfo <- read.csv("Student-K42-Info.csv", stringsAsFactors=FALSE)
+# names(studentinfo)[1:2] <- c("MSSV")
+# names(studentinfo)[c(5,34:36)] <- c("NgaySinh","DM1","DM2","DM3")
+# 
+# # Merge 2 data sets
+# fullstudent <- merge(diemthi, studentinfo, by = intersect(names(diemthi), names(studentinfo)), all=TRUE)
+# 
+# # Checked if students have the same Birthdate, DM1, DM2, DM3
+# sum(duplicated(fullstudent[,1:5])) # 14 cases
+# same <- fullstudent[(duplicated(fullstudent[,1:5]) | duplicated(fullstudent[,1:5], fromLast = TRUE)), ]
+# # ==> some students applied to 2 Majors, i.e. their names appears twice in the list of successful applications 
+# #and some students have the same Birthdate and DM1,DM2,DM3
+# 
+# # Checked if students in the list DiemThi appear in the student info data
+# 
+# # or vice versus
+# no_entrance <- fullstudent[is.na(fullstudent$HoLot),c(1:4,11,14,43,52:54)] # 41 students
+# sum(no_entrance$NÄfm.Tuyá.fn.sinh..TS. < 2016)
+
+
+# ------ Input the data K42 Diem Thi Dau Vao ------
+# Test how many type of entrance exams: 
+setwd("C:/Users/Vu/Google Drive/Ph.D/LMS")
+diemthi <- read_delim(file = "K42-Diem2.csv", delim=',')
+levels(as.factor(diemthi$`Môn 1`)) ## Only Math
+levels(as.factor(diemthi$`Môn 2`)) ## Physics and Liter
+levels(as.factor(diemthi$`Môn 3`)) ## Chemistry and English
+levels(as.factor(diemthi$Khoi))    ## 3 types
+names(diemthi)[19] <- "MSSV"
+names(diemthi)[16] <- "MaHS"
+
+# Input data student info
+studentinfo <- read.csv("Student-K42-Info.csv", stringsAsFactors=FALSE)
+names(studentinfo)[1] <- c("MSSV")
+names(studentinfo)[c(5,34:36)] <- c("NgaySinh","DM1","DM2","DM3")
+
+# Merge 2 data sets
+fullstudent <- merge(diemthi, studentinfo, by = "MSSV", all=TRUE)
+
+# Diemthi 4943 while fullstudent 4983, why having more 40 cases?
+sum(is.na(fullstudent[,2])) #  40 cases: 36 cases from K41, K40, but 4 cases from K42.
+no_diemthi <- fullstudent[is.na(fullstudent[,2]), ]
+
+# Check again using another merge
+fullstudent2 <- merge(diemthi, studentinfo, by = intersect(names(diemthi), names(studentinfo)), all=TRUE)
+sum(is.na(fullstudent2$STT)) # 43 cases do not appear in diemthi?
+#why
+test <- fullstudent2[is.na(fullstudent2$STT),]
+sum(test$NÄfm.Tuyá.fn.sinh..TS. < 2016) # 36 cases
+no_diemthi42 <- test[test$NÄfm.Tuyá.fn.sinh..TS. == 2016, ]
+# 7 cases with 2 of them is Du Bi, some of them appers 2 times in merged data
+# e.g 31161027023 in fullstudent2 have problem in NgaySinh
+sum(duplicated(fullstudent2$MSSV)) # 4 cases
+a <- fullstudent2[duplicated(fullstudent2$MSSV) | duplicated(fullstudent2$MSSV, fromLast = TRUE),]
+
+
+diemthi$ngay <- as.Date(diemthi$NgaySinh, format = "%d/%m/%y")
+diemthi$ngay2 <- strftime(diemthi$ngay, "%d/%m/%Y")
+
+b <- as.Date(diemthi$NgaySinh, format = "%d/%m/%y")
+diemthi$ngay <- b
+
+# Checked if students in the list DiemThi appear in the student info data
+
+# or vice versus
+no_entrance <- fullstudent[is.na(fullstudent$HoLot),] # 41 students
+sum(no_entrance$NÄfm.Tuyá.fn.sinh..TS. < 2016)

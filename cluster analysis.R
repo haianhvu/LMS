bce8fb88d0f6d836b1eaca72685c9c9e224b091e
YourUsername <haianhvu@ueh.edu.vn>
2019-07-05 14:51:38 +0200

    Attempt 1

--- cluster analysis.R
+++ cluster analysis.R
@@ -3,14 +3,29 @@ install.packages("dplyr")
 install.packages("foreign")
 install.packages("haven")
 install.packages("reshape2")
+install.packages("NbClust")
 
 library("readr")
 library("dplyr")
 library("foreign")
 library("haven")
 library(reshape2)
+library(NbClust)
+
 
 ######## Use file "factorK42-24major.dta" in the Script factoranalysis.R
 ######## because this is the main analysing file of the study.
 
-write_dta(full_general_score, "C:/Users/Vu/Google Drive/Ph.D/LMS/factorK42-24major.dta") 
+data <- read_dta("C:/Users/Vu/Google Drive/Ph.D/LMS/factorK42-24major.dta")
+nb <- NbClust(data[,4:12], distance = "euclidean",
+              min.nc=2, max.nc=10, method = "kmeans", index="all")
+# , diss="NULL"  : do not work if we put this argument into the command
+# max.nc=15 is the default,
+
+nb$All.index
+nb$All.CriticalValues
+nb$Best.partition
+nb$Best.nc -> a
+
+barplot(table(a[1,]))
+

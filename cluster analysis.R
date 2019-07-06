53c80aac2e36bce5690eb85fd3d2486c3f7fd5cc
YourUsername <haianhvu@ueh.edu.vn>
2019-07-06 13:12:06 +0200

    Only keep CTT

--- cluster analysis.R
+++ cluster analysis.R
@@ -11,13 +11,19 @@ library("foreign")
 library("haven")
 library(reshape2)
 library(NbClust)
+library(cluster)
+library(fpc)
 
 
 ######## Use file "factorK42-24major.dta" in the Script factoranalysis.R
 ######## because this is the main analysing file of the study.
 
 data <- read_dta("C:/Users/Vu/Google Drive/Ph.D/LMS/factorK42-24major.dta")
-nb <- NbClust(data[,4:12], distance = "euclidean",
+data1 <- data[data$specialmajor==0 & data$highqualtymajor==0,]
+d <- data1[,4:12]
+dat <- na.omit(d)
+
+# nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "ward.D2", index="all")
 # , diss="NULL"  : do not work if we put this argument into the command
 # max.nc=15 is the default,
@@ -30,27 +36,30 @@ nb$Best.nc -> a
 table(a[1,])
 barplot(table(a[1,])
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
        min.nc=2, max.nc=10, method = "ward.D2", index="all", ) # best is 4
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
       min.nc=2, max.nc=10, method = "ward.D", index="all") # 3
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "kmeans", index="all") # 2
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "complete", index="all") # 2, 4
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "single", index="all") # 3
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "median", index="all") # 2
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "centroid", index="all") # 2
 
-nb <- NbClust(data[,4:12], distance = "euclidean",
+nb <- NbClust(dat, distance = "euclidean",
               min.nc=2, max.nc=10, method = "average", index="all") # 2
+
+cluster1 <- kmeans(d, 3)  # error due to missing values?
+
 

63386339c9093e47241b53e253e178f598922ac3
YourUsername <haianhvu@ueh.edu.vn>
2019-07-28 15:03:12 +0200

    OK

--- esem.R
+++ esem.R
@@ -10,6 +10,7 @@ library(psych)
 rm(list=ls())
 
 data <- read_dta("C:/Users/Vu/Google Drive/Ph.D/LMS/K43/factor43-24major-deleteK40.dta")
+# Use this data because K43 CTT has some students of other intakes
 data1 <- sapply(data,as.numeric)
 data1 <- as.data.frame(data1)
 data2 <- data1[,c(2:10)]
@@ -36,9 +37,12 @@ fitmeasures(b5.cfa, c("cfi.robust","tli.robust","rmsea.robust","srmr"))
 
 # ------- let's try ESEM! First, we conduct an exploratory FA on the dataset -------
 # Try with K43 data  
+parallel <- fa.parallel(data2, fm = 'pa', fa = 'fa')
+
 b5.efa <- fa(data2, nfact = 3, rotate = "geominQ", fm = "ml")
 b5.efa <- fa(data2, nfact = 3, rotate = "promax", fm = "pa")
 b5.efa
+
 print(b5.efa$loadings,cutoff = 0.1)
 fa.diagram(b5.efa)
 
@@ -51,8 +55,10 @@ dt3 <- dt2[,4:12]
 parallel <- fa.parallel(dt3, fm = 'pa', fa = 'fa')
 b5.efa <- fa(dt3, nfact = 3, rotate = "promax", fm = "pa")
 b5.efa
+
+print(b5.efa$loadings,cutoff = 0.29) # results are nearly similar to stata
 fa.diagram(b5.efa)
-# results are nearly similar to stata
+
 
 #
 

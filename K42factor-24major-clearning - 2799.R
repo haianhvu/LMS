622cf70fb03770aea2e3fde53be7c857a6048efd
YourUsername <haianhvu@ueh.edu.vn>
2019-07-28 15:07:39 +0200

    OK

--- K42factor-24major-clearning - 2799.R
+++ K42factor-24major-clearning - 2799.R
@@ -70,3 +70,61 @@ setdiff(test3$MSSV,dt2$MSSV) #many MSSV exist in test3, not dt2
 # reason: some MSSV study 2 major, 1 HPR and 1 CTT, so they still appear in folder CTT24
 # the best way is combine all CTT, HPR, CLC, and then eliminate duplicated
 # so the correct data of K42 should be found in final.R
+
+#------- Above is 3117 students appearing in CTT, but maybe ------
+# they also study HPR or CLC. Check and eliminate these students
+
+setwd("C:/Users/Vu/Google Drive/Ph.D/LMS/K43")
+# 6 CLC, 4 HPR for each semester
+
+pattern <- "(CLC|CTR)_((HKD_2018)|(HKC_2017))"
+Files <- list.files(path = "C:/Users/Vu/Google Drive/Ph.D/LMS/K43", pattern=pattern)
+data <- lapply(Files, function(x) read.csv(x, stringsAsFactors = FALSE)) # Xem tren data de biet bao nhieu file
+#sum(sapply(data,length))
+
+MSSV <- sapply(data, function(x) (x[-c(1,2),1]))
+MSSV1 <- as.data.frame(unlist(MSSV))
+names(MSSV1) <- "MSSV"
+MSSV2 <- MSSV1 %>% group_by(MSSV) %>% mutate(count = row_number(MSSV))
+MSSV3 <- unique(MSSV1) #1125
+table(MSSV2$count)
+
+score43ff[score43ff$StudentID %in% MSSV3$MSSV,1] # 5 obs
+intersect(MSSV3$MSSV,score43ff$StudentID)
+MSSV3[MSSV3$MSSV %in% score43ff$StudentID,1] # 5 obs. Data with 1125 factor levels
+
+score43fff <- score43ff[!score43ff$StudentID %in% MSSV3$MSSV,]
+score43fff <- score43fff[grepl("3117",score43fff$StudentID),] #3109
+write_dta(score43fff[,-(2:3)], "C:/Users/Vu/Google Drive/Ph.D/LMS/K43/factor43-24major-3109.dta") 
+
+
+###### Try EFA with score43fff
+data <- score43fff
+data1 <- sapply(data,as.numeric)
+data2 <- data1[,c(4:12)]
+
+# ------- First, we perform a CFA and look at the fit measures and loadings -----
+
+b5.model <- "
+MATH =~ ECO501001 + ECO501002 + MAT508001 + ACC507001
+VERBAL =~  PML510001 + PML510002 + LAW511001
+LANG =~ ENG513001 + ENG513002
+"
+
+b5.cfa <- cfa(b5.model, data = data2, estimator = "MLR")
+# Marsh et al. (2013) fit measures:
+# CFI: .761, TLI: .687, RMSEA: .076
+# Our fit measures:
+fitmeasures(b5.cfa, c("cfi.robust","tli.robust","rmsea.robust","srmr"))
+
+# ------- let's try ESEM! First, we conduct an exploratory FA on the dataset -------
+# Try with K43 data  
+parallel <- fa.parallel(data2, fm = 'pa', fa = 'fa')
+
+b5.efa <- fa(data2, nfact = 3, rotate = "geominQ", fm = "ml")
+b5.efa <- fa(data2, nfact = 3, rotate = "promax", fm = "pa")
+b5.efa
+
+print(b5.efa$loadings,cutoff = 0.1)
+fa.diagram(b5.efa)
+

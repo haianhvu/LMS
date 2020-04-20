8e555615946021dc372e556a4450fbcc483b36d7
YourUsername <haianhvu@ueh.edu.vn>
2020-04-20 11:58:22 +0200

    a

--- scholarship.R
+++ scholarship.R
@@ -151,7 +151,7 @@ hk2 <- hk2[,-1]
 levels(as.factor(hk1$S·...t√.n.ch·..)) ## All of them had 15 (maybe first semester)
 levels(as.factor(hk2$S·...t√.n.ch·..)) ## All of them had 13. why?
 
-# howmany students
+# how many students
 length(unique(hk1$MSSV)) ### 3177 / 3434. 257 duplicated
 sum(duplicated(hk1$MSSV))
 length(unique(hk2$MSSV)) ### 3126 / 3385. 259 duplicated
@@ -162,15 +162,31 @@ hk1a <- unique(hk1) ### 3182 students. Still having 5 MSSV duplicated
 sum(duplicated(hk1a$MSSV)) #Still having 5 MSSV duplicated
 hk1a[duplicated(hk1a$MSSV),][,1]
 hk1a <- hk1a[!(duplicated(hk1a$MSSV) | duplicated(hk1a$MSSV, fromLast = TRUE)),]
+# delete both duplicated and original record of duplicated (because do 
+# not know which one should be kept)
 
 hk2a <- unique(hk2) ### 3134. 
 sum(duplicated(hk2a$MSSV)) # Still having 8 MSSV duplicated
 hk2a[duplicated(hk2a$MSSV),][,1]
+t <- hk2a %>% filter(duplicated(hk2a$MSSV) | duplicated(hk2a$MSSV, fromLast = T))
+t <- hk2 %>% filter(duplicated(hk2$MSSV) | duplicated(hk2$MSSV, fromLast = T))
+t1 <- t %>% count(MSSV) 
+t1 %>% count(n)
 
 # Delete Students of BI, AV, AE major: 2839 students
 removelist <- c('AV', "AE", 'BI')
 hk1final <- hk1a[ !grepl(paste(removelist, collapse="|"), hk1a$L·..p), ]
 # This is the "Nhom 4" Scholarship: 7.77 / 79
+# test with hk1:
+hk1final1 <- hk1[ !grepl(paste(removelist, collapse="|"), hk1$L·..p), ]
+t <- hk1final1 %>% filter(duplicated(MSSV) | 
+                            duplicated(MSSV, fromLast = T)) # 236
+t1 <- unique(hk1final1) # 2843
+t2 <- t1 %>% filter(duplicated(MSSV) | 
+                             duplicated(MSSV, fromLast = T)) 
+# 4 obs: coincide with first way because 2843 - 4 = 2839
+# conclude: delete all of duplicated data to make sure that everything is normal
+# and there is nothing special.
 
 ###### Tabula
 table(hk1final[hk1final$ƒ.i·.fm.TBCHT >= 7.77,]$X·∫.p.lo·∫.i.r√.n.luy·..n)
